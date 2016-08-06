6c41cab3a43695da880184336eb6d9274f20f357
Russell Wenban <russell@factornine.co.uk>
2016-08-06 16:14:55 +0100

    - Update documentation –  Reorder app – Add stub directive

--- README.md
+++ README.md
@@ -1,8 +1,63 @@
-# Webstorm Presentation
+#
 
 The intention of this project to create a "Real World" project where the many features of IntelliJ/Webstorm can be demonstrated by "Live Coding" examples.
  
 ## Project Introduction
 
-Using the very basic angular-seed as a base, there is no build process to distribute files to a distribution directory.
+This project is using the very basic angular-seed as a base, there is no build process to distribute files to a build directory.
+
+
+### Clone Webstorm Presentation
+
+Clone the angular-seed repository using [git][git]:
+
+```
+git clone https://github.com/russellf9/webstorm-presentation.git
+cd webstorm-presentation
+```
+
+### Install Dependencies
+
+```
+npm install
+```
+
+### Set up a JavaScript Debug Configuration
+
+
+For Chrome Install the [JetBrains IDE Support](https://chrome.google.com/webstore/detail/jetbrains-ide-support/hmhgeddbohgjknpmjagkdomcpobmllji?hl=en)
+
+Select `+` and "Add a new JavaScript Debug Config"
+
+Set the following:
+
+Name - Webstorm Presentation
+URL - http://127.0.0.1:8000/
+
+
+### Run the Application
+
+```
+npm start
+```
+A browser tab will open the app aat `http://localhost:8000/index.html`.
+
+
+### Launch the Debug Configuration
+
+Select ^D or press the green bug button
+
+Now browse to the app at `http://127.0.0.1:8000/`
+
+(This is the URL we need to view to test the app rather than the localhost URL.)
+
+
+### Tasks TODO
+
+[ ] Get the debugger running
+[ ] 
+
+
+### App TODO
+[ ] Fix Directive Live Template
 
---
+++ app/components/people/peopleList.directve.js
@@ -0,0 +1,61 @@
+(function () {
+
+    'use strict';
+
+
+    /**
+     *
+     * @author russell
+     * @date 06/08/2016
+     *
+     * @name
+     * myApp:peopleList
+     *
+     * @param {data} input An input two-way binding
+     * @param {function} updateFn An output function
+     * 
+     * @description Provides a UI to hold a list of people
+     * Trying to follow the style guidelines from: {https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md#angular-docs Angular 1 Style Guide <br>
+     * @example
+        <pre>
+         <people-list
+            input='data'
+            update-fn='update'>
+         <people-list/>
+        <pre>
+     *
+     */
+
+    var module;
+
+    try {
+        module = angular.module('myApp.people.list');
+    } catch (err) {
+        module = angular.module('myApp.people.list', [])
+    }
+    
+    module.directive('peopleList', peopleList);
+
+    function peopleListController($log) {
+    
+        function _callUpdate() {
+            if (typeof $scope.updateFn == 'function') {
+                $scope.updateFn()();
+            } else {
+                $log.error('An update function is required!');
+            }
+        }
+    }
+
+    function peopleList() {
+        return {
+            restrict: 'AE',
+            scope: {
+                input: '=?',
+                updateFn: '&?'
+            },
+            templateUrl: 'components/people/people_list.html',
+            controller: peopleListController
+        }
+    }
+}());
